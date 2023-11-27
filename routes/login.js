6ba977b9dfec86a4de83803e8cc7474c3497a10c
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-11-27 11:35:59 -0500

    back to  normal

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const User = require('./models/User'); // Import your User model
+const User = require('../models/User'); // Import your User model
 const bcrypt = require('bcrypt');
 
 router.post('/', async (req, res) => {
